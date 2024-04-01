c06acd4e0a863567b9a0445a3daaa43e357023d7
Alexandre <aborgesgoncalves@gmail.com>
2024-04-01 16:04:31 -0300

    1.0

---
+++ view/cadastroView.php
@@ -0,0 +1,39 @@
+<html>
+<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="cadastro.css">
+<body>
+    
+
+<div id="" class="formsCadastro">
+    <form action="" id="Cadastro">
+        <h1>Cadastro de produtos</h1>
+        <div class="input1">
+            <label for="">Nome:</label>
+            <input class="inputNameDescricao" type="text" name="produto">
+            <label for="">Descrição:</label>
+            <input  class="inputNameDescricao" type="text" name="descricao">
+        </div>
+        <div class="input2">
+            <label for="">Quantidade:</label>
+            <input type="number" name="quantidade">
+            <label for="">Valor:</label>
+            <input type="number" name="valor">
+        </div>
+        <div class="input3"> 
+            <label for="">Categoria:</label>
+            <select name="" id="">
+            <option selected></option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+            </select>
+        </div>
+        <div class="btns">
+        <input type="submit" value="Editar">
+        <input type="submit" value="Salvar">
+        <input type="submit" value="Cancelar">        
+        </div>
+    </form>
+</div>
+</body>
+</html>
